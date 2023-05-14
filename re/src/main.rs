2db2f637b33fc67fc951d88c57fe4d0e3c38aad2
liam <liam.henrickson@gmail.com>
2023-05-14 15:38:14 -0600

    re: Add '--name_only' flag, refactor, & fix '--create_ancestors' bug

--- re/src/main.rs
+++ re/src/main.rs
@@ -1,10 +1,11 @@
 // TODO:
 //  - improve errors
+//  - tell user that ancestor doesn't exist & suggest nearest name (obv if one doesn't exist)
+//      this should probably be mostly done by an separate crate.
 
-use std::borrow::Cow;
 use std::fs;
 use std::io::{self, Read, Seek, Write};
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use std::process::Command;
 
 use anyhow::{bail, Context, Result};
@@ -14,38 +15,35 @@ use tempfile::NamedTempFile;
 
 const EDITOR: &str = "hx";
 
-/// Rename a file, opening the old name in an editor if one isn't provided.
+/// Rename a file, opening the old name in an editor if one isn't provided
 #[derive(Parser, Debug)]
 struct Args {
     src: PathBuf,
     dst: Option<PathBuf>,
-    /// In the editor, use the fully qualified, canonical path instead of the relative path.
+    /// In the editor, use the fully qualified, canonical path instead of the relative path
     #[arg(short = 'c', long)]
     canonical: bool,
-    /// Replace the destination file if it already exists.
+    /// Open to only the file name in the editor
+    #[arg(short = 'n', long, conflicts_with = "canonical")]
+    name_only: bool,
+    /// Replace the destination file if it already exists
     #[arg(short = 'd', long)]
     destructive: bool,
-    /// Create ancestor directories of destination if they don't already exist.
+    /// Create ancestor directories of destination if they don't already exist
     #[arg(short = 'a', long)]
     create_ancestors: bool,
 }
 
-fn get_dst_in_editor(src: &Path, use_canonical: bool) -> Result<PathBuf> {
+fn get_dst_in_editor(placeholder: &str) -> Result<PathBuf> {
     // Apparently the rust interface for windows paths uses utf8 even though windows used wtf16.
     // Even though `std::os::windows::ffi::OsStrExt` suggests otherwise. I spent way too long trying
     // to juggle the encoding back and forth before I got to the point where it said windows paths
     // can't contain NULs.
 
-    // Handle canonicalizing
-    let mut src = Cow::Borrowed(src);
-    if use_canonical && src.is_relative() {
-        src = Cow::Owned(std::env::current_dir()?.join(src).clean());
-    }
-
     // Make the file and write the placeholder
     let mut tmp_file = NamedTempFile::new().context("Failed to make temp file")?;
     tmp_file
-        .write_all(src.to_string_lossy().as_bytes())
+        .write_all(placeholder.as_bytes())
         .context("Failed to write placeholder to temp file")?;
 
     // Open the editor
@@ -66,17 +64,38 @@ fn get_dst_in_editor(src: &Path, use_canonical: bool) -> Result<PathBuf> {
 }
 
 fn run(args: Args) -> Result<()> {
-    if !args.src.exists() {
-        bail!("\"{}\" doesn't exist", args.src.display());
+    let mut src = args.src;
+
+    if !src.exists() {
+        bail!("\"{}\" doesn't exist", src.display());
     }
 
-    let dst = match args.dst {
-        Some(dst) => dst,
-        None => get_dst_in_editor(&args.src, args.canonical)?,
-    };
+    if args.canonical && src.is_relative() {
+        src = std::env::current_dir()?.join(src).clean();
+    }
 
+    let mut dst = match args.dst {
+        Some(dst) => dst,
+        None => {
+            let placeholder = if args.name_only {
+                src.file_name()
+                    .context("`name-only` flag used with source path that ends in `..`")?
+            } else {
+                src.as_os_str()
+            }
+            .to_string_lossy();
+
+            get_dst_in_editor(&placeholder)?
+        }
+    };
     if dst.as_os_str().is_empty() {
         bail!("Destination can't be an empty string");
+    }
+    if args.name_only {
+        dst = src
+            .parent()
+            .context("'name-only' flag used with invalid path")?
+            .join(dst);
     }
     if !args.destructive && dst.exists() {
         bail!(
@@ -85,11 +104,13 @@ fn run(args: Args) -> Result<()> {
         );
     }
     if args.create_ancestors {
-        fs::create_dir_all(&dst).context("Failed to create ancestors")?;
+        if let Some(p) = dst.parent() {
+            fs::create_dir_all(&p).context("Failed to create ancestors")?
+        }
     }
 
-    if args.src != dst {
-        Ok(std::fs::rename(args.src, dst).context("Failed to rename")?)
+    if src != dst {
+        Ok(std::fs::rename(src, dst).context("Failed to rename")?)
     } else {
         Ok(())
     }
